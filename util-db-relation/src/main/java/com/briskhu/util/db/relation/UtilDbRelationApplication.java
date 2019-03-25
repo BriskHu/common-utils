b3188547e757648f25b0171c4769594afd98d4b5
Brisk <number9707@163.com>
2019-03-25 21:31:06 +0800

    A project for some common and often used utilities

---
+++ util-db-relation/src/main/java/com/briskhu/util/db/relation/UtilDbRelationApplication.java
@@ -0,0 +1,13 @@
+package com.briskhu.util.db.relation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilDbRelationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilDbRelationApplication.class, args);
+    }
+
+}
---
+++ util-http/src/main/java/com/briskhu/util/http/UtilHttpApplication.java
@@ -0,0 +1,13 @@
+package com.briskhu.util.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilHttpApplication.class, args);
+    }
+
+}
